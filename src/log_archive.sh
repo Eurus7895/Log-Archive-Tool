5933c65a7d1db5e88eaa664944277a846f9be1e6
Hoang Nguyen <t.hoang7895@gmail.com>
2025-10-16 23:50:39 +0000

    doc: Add description to improve readability

--- src/log_archive.sh
+++ src/log_archive.sh
@@ -2,22 +2,32 @@
 
 compress_list_file(){
 	directory=$1
+	# Check if the input exist
 	if [ ! -e "$directory" ]; then
 		echo "Input is incorrect"
+		# Return to 0 to inhibit more error message 
 		return 0
 	fi
 	
+	# Check if the input is directory as It can be a file
 	if [ ! -d "$directory" ]; then
 		echo "Input is not a directory"
 		return 0
 	fi	
 	
+	# Check the string returned from ls is empty or not
 	if [ -z $(ls $directory) ]; then
 		echo "Directory is empty"
 		return 0
 	fi
 	
 	output_name="logs_$(date +%Y%m%d_%H%M%S).tar.gz"
+
+	# Archive and compress the folder
+	# -c: create a new archive
+	# -z: compress file by gzip
+	# -v: show information of the file will be compressed
+	# -f: choose the output file name
 	tar -czvf $output_name -C $directory .
 	
 	if [ ! -e $output_name ]; then
