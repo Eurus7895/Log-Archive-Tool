bb44abf6f625d679639ed2e116b002a149e17dd4
Hoang Nguyen <t.hoang7895@gmail.com>
2025-10-16 23:19:06 +0000

    chore: Check for incorrect input

--- src/log_archive.sh
+++ src/log_archive.sh
@@ -2,21 +2,30 @@
 
 compress_list_file(){
 	directory=$1
+	if [ ! -e "$directory" ]; then
+		echo "Input is incorrect"
+		return 0
+	fi
+	
 	if [ ! -d "$directory" ]; then
 		echo "Input is not a directory"
+		return 0
 	fi	
 	
 	if [ -z $(ls $directory) ]; then
 		echo "Directory is empty"
+		return 0
 	fi
 	
 	output_name="logs_$(date +%Y%m%d_%H%M%S).tar.gz"
 	tar -czvf $output_name -C $directory .
 	
-	if [ ! -e $  ]; then
+	if [ ! -e $output_name ]; then
 		echo "Fail to create ${output_name}"
+		return 0
 	fi
 	echo "Compress form ${$directory} to ${output_name} successfully"
+	return 1
 }
 
 
